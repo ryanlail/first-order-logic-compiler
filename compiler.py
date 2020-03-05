c09cec8534c3f949401b1948afc8550008267daa
Ryan <rlail1607@gmail.com>
2020-03-05 20:54:21 +0000

    arg parse working

--- compiler.py
+++ compiler.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 class LanguageDefinition:
@@ -11,20 +12,30 @@ class LanguageDefinition:
         self.quantifiers = []
         self.formula = ""
 
-    def read_input(self):
-        input_file_name = sys.argv[1]
-
+    def read_input(self, input_file_name):
         with open(input_file_name) as fh:
             lines = fh.read().splitlines()
 
+        #for line in lines:
 
 
 
 
+
+
+def arg_parser():
+    #  parse the arugments from the command line NOT the compiler parser
+    parser = argparse.ArgumentParser(description="A Lexical Analyser & Parser for First-Order Logic")
+    parser.add_argument("-i","--input_file_name", type = str, default = "example.txt", help = "Name of the input file to read language definitons and formula from")
+   # parser.add_argument("parse_tree_file_name", type = str, default = "tree.png", help = "Name of the output parse tree")
+   # parser.add_argument("log_file_name", type = str, default = "log.txt", help = "Name of the log file")
+
+    return parser.parse_args()
 
 def main():
-    input_from_file = input()
-    input_from_file.read_input()
+    arguments = arg_parser()
+    input_from_file = LanguageDefinition()
+    input_from_file.read_input(arguments.input_file_name)
 
 if __name__ == "__main__":
     main()
