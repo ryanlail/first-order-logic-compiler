65792a5762a9eb69920b259daf52c4dc28158e9a
Ryan <rlail1607@gmail.com>
2020-03-05 18:39:45 +0000

    input parsed

--- compiler.py
+++ compiler.py
@@ -1,16 +1,30 @@
-class input:
+import sys
+
+class LanguageDefinition:
 
     def __init__(self):
         self.variables = []
         self.constants = []
         self.predicates = [] #  2d array e.g. [[p,2],[q,1]]
-        self.equality = ''
+        self.equality = ""
         self.connectives = []
         self.quantifiers = []
+        self.formula = ""
+
+    def read_input(self):
+        input_file_name = sys.argv[1]
+
+        with open(input_file_name) as fh:
+            lines = fh.read().splitlines()
+
+
+
+
 
 
 def main():
-    return 1;
+    input_from_file = input()
+    input_from_file.read_input()
 
 if __name__ == "__main__":
     main()
