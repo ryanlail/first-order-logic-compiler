23e9bc883f81d48b57504d589c69985c47f993cf
Ryan <rlail1607@gmail.com>
2020-03-05 18:19:15 +0000

    basic class structure

---
+++ compiler.py
@@ -0,0 +1,16 @@
+class input:
+
+    def __init__(self):
+        self.variables = []
+        self.constants = []
+        self.predicates = [] #  2d array e.g. [[p,2],[q,1]]
+        self.equality = ''
+        self.connectives = []
+        self.quantifiers = []
+
+
+def main():
+    return 1;
+
+if __name__ == "__main__":
+    main()
