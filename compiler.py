f9cd0260ddcad2af191659a68f925fe83d037ed4
Ryan <rlail1607@gmail.com>
2020-03-10 10:46:55 +0000

    grammar output

--- compiler.py
+++ compiler.py
@@ -191,6 +191,21 @@ class Grammar:
             variables_rule += variable + "|"
         self.productions.append(variables_rule[:-1])
 
+    def output(self, file_name):
+        with open(file_name, "w") as fh:
+            terminal_output = ""
+            terminal_output += "Terminal Symbols: {"
+            for terminal in self.terminals:
+                terminal_output += terminal + ", "
+            terminal_output = terminal_output[:-2]
+            terminal_output += "}\n"
+            fh.write(terminal_output)
+            fh.write("Non-terminal Symbols: " + str(self.non_terminals) + "\n")
+            fh.write("Production Rules: \n")
+            for rule in self.productions:
+                fh.write(rule + "\n")
+            fh.write("Start Symbol: " + self.start_symbol + "\n")
+
 
 
 
@@ -198,7 +213,10 @@ class Grammar:
 def arg_parser():
     #  parse the arugments from the command line NOT the compiler parser
     parser = argparse.ArgumentParser(description="A Lexical Analyser & Parser for First-Order Logic")
-    parser.add_argument("-i","--input_file_name", type = str, default = "example.txt", help = "Name of the input file to read language definitons and formula from")
+    parser.add_argument("-i","--input_file_name", type = str, default = "example.txt",
+            help = "Name of the input file to read language definitons and formula from")
+    parser.add_argument("-g", "--grammar_file_name", type = str, default = "grammar.txt",
+            help = "Name of the output file for the definition of the grammar")
    # parser.add_argument("parse_tree_file_name", type = str, default = "tree.png", help = "Name of the output parse tree")
    # parser.add_argument("log_file_name", type = str, default = "log.txt", help = "Name of the log file")
 
@@ -209,6 +227,7 @@ def main():
     input_from_file = LanguageDefinition()
     input_from_file.read_input(arguments.input_file_name)
     new_grammar = Grammar(input_from_file)
+    new_grammar.output(arguments.grammar_file_name)
 
 
 if __name__ == "__main__":
